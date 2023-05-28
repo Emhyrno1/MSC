3b1b65b57a1ff957c2e54c65ab1b0a7320ea1964
Emhyrno1 <134741520+Emhyrno1@users.noreply.github.com>
2023-05-28 06:26:07 +0330

    Update Centos.sh

--- Centos.sh
+++ Centos.sh
@@ -1,29 +1,36 @@
 #!/bin/bash
 
-# Check if script is running as root
+# Check if script is running as root, if not, exit.
 if [[ $EUID -ne 0 ]]; then
    echo "This script must be run as root" 
    exit 1
 fi
 
-# Update and upgrade the system
+# Update the system and install necessary packages
 yum update -y
+yum install wget curl -y
+yum install epel-release -y
+yum install iproute -y
+yum install docker -y
 
-# Install required packages
-yum install -y wget curl tc iptables-services
-
-# Install Docker
-yum install -y yum-utils
-yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
-yum install -y docker-ce docker-ce-cli containerd.io
+# Start docker service
 systemctl start docker
+
+# Enable docker service
 systemctl enable docker
 
 # Get the primary network interface
 INTERFACE=$(ip route | grep default | sed -e "s/^.*dev.//" -e "s/.proto.*//")
 
-# Set the qdisc to fq_codel for the primary network interface
-tc qdisc add dev ${INTERFACE} root fq_codel
+# Set the qdisc to fq_codel for the primary network interface, if tc is available
+if command -v tc >/dev/null 2>&1; then
+    tc qdisc add dev ${INTERFACE} root fq_codel
+else
+    echo "tc utility not found, skipping setting qdisc to fq_codel"
+fi
+
+# Install BBR
+wget --no-check-certificate https://github.com/teddysun/across/raw/master/bbr.sh && chmod +x bbr.sh && bash bbr.sh
 
 # Configure sysctl parameters
 sed -i '/net.core.default_qdisc/d' /etc/sysctl.conf
@@ -35,16 +42,11 @@ echo 'net.ipv4.tcp_congestion_control=bbr' >> /etc/sysctl.conf
 sed -i '/net.ipv4.tcp_fastopen/d' /etc/sysctl.conf
 echo 'net.ipv4.tcp_fastopen=3' >> /etc/sysctl.conf
 
-# Install BBR
-wget -N --no-check-certificate https://github.com/teddysun/across/raw/master/bbr.sh
-chmod +x bbr.sh
-bash bbr.sh
-
 # Apply the changes
 sysctl -p
 
 # Run the install script from GitHub three times
-for i in 1 2 3
+for i in {1..3}
 do
     source <(curl -L https://github.com/trojanpanel/install-script/raw/main/archive/install_script_v2.0.5.sh)
 done
